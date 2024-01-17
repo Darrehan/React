a1355b44c39251d9cd6bbc6f277f48c835286a09
Darrehan <darrehanrasool@gmail.com>
2024-01-17 16:33:33 +0530

    Dar Rehan Rasool
    sycomusiclover

--- React-1/03 React Essentials/01-starting-project/01-starting-project/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-import App from "./App.jsx";
-import "./index.css";
-
-const entryPoint = document.getElementById("root");
-// its rendering the content from App.jsx to the entryPoint
-ReactDOM.createRoot(entryPoint).render(<App />);
--- React-2/05 Essentials Practice/01-starting-project/src/App.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
-}
-
-export default App
