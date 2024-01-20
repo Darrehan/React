edbc77db9c5164ecad9dcce8b4ec61a406e9de13
Darrehan <darrehanrasool@gmail.com>
2024-01-21 00:25:03 +0530

    Dar Rehan Rasool
    sycomusiclover

---
+++ Login component/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Login from "../component/Login";
+import "./App.css"
+
+var isLoggedIn = false;
+const currentTime = new Date(2024, 11, 1, 9).getHours();
+console.log(currentTime);
+
+function App() {
+  return (
+    <div className="container">
+  
+     <Login
+     logedin={isLoggedIn}
+     currentTime={currentTime}
+      />
+    
+    </div>
+  );
+}
+
+export default App;
