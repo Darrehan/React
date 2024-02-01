0f822f9796199105f97a98a96323615062961445
Dar Rehan Rasool <95292119+Darrehan@users.noreply.github.com>
2024-02-02 02:47:23 +0530

    Delete Login directory

--- Login/component/input.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import React from "react";
-
-function Input(props) {
-  return <input type={props.type} placeholder={props.placeholder} />;
-}
-
-export default Input;
